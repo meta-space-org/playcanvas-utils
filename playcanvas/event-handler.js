ff0201cbdfc01efae271fad5bf1ec0d22d710eb8
mrmaxm <core@moka.co>
2023-07-18 12:29:01 +0300

    Create event-handler.js

---
+++ playcanvas/event-handler.js
@@ -0,0 +1,34 @@
+// Modify pc.EventHandler so it returns an object with `off` method for easier events detachment.
+// If events chaning were ever used in the project, this will be conflicting change.
+
+pc.EventHandler.prototype._addCallback = function (name, callback, scope, once = false) {
+    if (!name || typeof name !== 'string' || !callback)
+        return;
+
+    if (!this._callbacks[name])
+        this._callbacks[name] = [];
+
+    if (this._callbackActive[name] && this._callbackActive[name] === this._callbacks[name])
+        this._callbackActive[name] = this._callbackActive[name].slice();
+
+    let obj = {
+        callback: callback,
+        scope: scope || this,
+        once: once,
+        off: () => {
+            this.off(name, callback, scope || this);
+        }
+    };
+
+    this._callbacks[name].push(obj);
+
+    return obj;
+};
+
+pc.EventHandler.prototype.on = function (name, callback, scope) {
+    return this._addCallback(name, callback, scope, false);
+};
+
+pc.EventHandler.prototype.once = function (name, callback, scope) {
+    return this._addCallback(name, callback, scope, true);
+};
